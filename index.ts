4e8805f1d48a49e51790c30501a268e79daab38d
Pablo-gg9 <pgordogonzalez@gmail.com>
2026-01-20 11:28:10 +0100

    Añado endpoint

--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
 import { initDB} from "./db/init";
 import { getAllMovies,getMoviesById,getMoviesByTitle } from "./models/movies";
+import express from "express"
 
+const app =express();
+const PORT= 3000
 const db = await initDB()
+
+app.get("/movies",(req,res)=>{
+    const peliculas = getAllMovies(db)
+    res.json(peliculas)
+})
+app.listen(PORT,()=>{
+    console.log("Servidor funcionando en puerto "+PORT)
+})
+
 // const peliculas = getAllMovies(db)
 // console.log(peliculas)
 const pelicula= getMoviesById(db,35)
